bc5814eef3d245531cc7c3aae66c0b9bcfc77669
Megaache Younes <36759976+megaacheyounes@users.noreply.github.com>
2023-04-02 10:33:20 +0400

    npm publish attempt #4 -_-

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,15 @@
 import Bananalyzer from 'bananalyzer';
 
-const dl = await Bananalyzer.getDownloadLink('com.aswat.carrefouruae');
-console.log('dl', dl);
-const result = await Bananalyzer.analyzeAPKs(
-  [
-    {
-      filePath: './tests/samples/sample.apk',
-    },
-  ],
-  true
-);
-console.log('result', result);
+async () => {
+  const dl = await Bananalyzer.getDownloadLink('com.aswat.carrefouruae');
+  console.log('dl', dl);
+  const result = await Bananalyzer.analyzeAPKs(
+    [
+      {
+        filePath: './tests/samples/sample.apk',
+      },
+    ],
+    true
+  );
+  console.log('result', result);
+};
