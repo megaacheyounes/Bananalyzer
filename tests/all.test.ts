ce07b6d5276183d3aa2dac92bebbf897d034334f
Megaache Younes <36759976+megaacheyounes@users.noreply.github.com>
2023-04-24 23:28:30 +0400

    Fix tsJest not running tests in order

---
+++ tests/all.test.ts
@@ -0,0 +1,10 @@
+//to run tests in sorted order
+
+import './appDetailsScrapper.test';
+import './downloader.test';
+import './decompile.test';
+import './parseManifest.test';
+import './analyzeTrackingSdk.test';
+import './analyzer.test';
+import './utils.test';
+import './excelHelper.test';
